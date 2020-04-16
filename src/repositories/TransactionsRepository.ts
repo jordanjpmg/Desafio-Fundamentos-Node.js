05f0cd00a2a0e719b09775a0de1f0fc97eabc68d
jordanjpmg <jordanjpmg@gmail.com>
2020-04-16 16:06:12 -0300

    Challenge Concluded

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransaction {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,39 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions
+      .filter(type => type.type === 'income')
+      .reduce((acc, cur) => acc + cur.value, 0);
+    const outcome = this.transactions
+      .filter(type => type.type === 'outcome')
+      .reduce((acc, cur) => acc + cur.value, 0);
+    const total = income - outcome;
+    const balance = {
+      income,
+      outcome,
+      total,
+    };
+    if (outcome > total) {
+      throw Error('Saldo insuficiente');
+    } else {
+      return balance;
+    }
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: CreateTransaction): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    const balance = this.getBalance();
+
+    if (balance.outcome > balance.total) {
+      throw Error('Saldo insuficiente');
+    } else {
+      return transaction;
+    }
   }
 }
 
